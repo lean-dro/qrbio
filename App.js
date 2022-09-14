b1380a64a659c66ff31c527f564d9edb50c4428e
lean-dro <72211521+lean-dro@users.noreply.github.com>
2022-09-13 22:36:58 -0300

    Tentativa

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { Linking } from "react-native";
+import styles from "./assets/css/style";
+import { useFonts } from "expo-font";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import "react-native-gesture-handler";
 
-export default function App() {
+function Dev({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        onPress={() => navigation.navigate("Scanner")}
+        title="Go to notifications"
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Scanner({ navigation }) {
+ 
+
+  return (
+    <View style={styles.container}>
+      <Button
+        onPress={() => navigation.navigate("Dev")}
+        title="Go to notifications"
+      />
+      <Text style={{ fontFamily: "Merienda Bold", fontSize: 30 }}>
+        Leitor de QR-Code
+      </Text>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Dev">
+        <Drawer.Screen name="Scanner" component={Scanner} />
+        <Drawer.Screen name="Dev" component={Dev} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+// expo install expo-barcode-scanner
+// npx expo install expo-font
+// expo install expo-permissions
+
+// npm install @react-navigation/native
+// npx expo install react-native-screens react-native-safe-area-context
+// npm install @react-navigation/drawer
+// npx expo install react-native-gesture-handler react-native-reanimated
---
+++ assets/css/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    flexDirection: 'column',
+  },
+  titulo: {},
+  scanner: {
+    flex: 0.5,
+    height: 300,
+    width: "100%",
+  },
+  botao: {
+    marginTop: 30
+  },
+});
