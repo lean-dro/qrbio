c52ddf5e0d9caf58257ad0aace6a8707dc484b81
lean-dro <72211521+lean-dro@users.noreply.github.com>
2022-09-14 11:29:23 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -6,6 +6,25 @@ import styles from "./assets/css/style";
 import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+const [hasPermission, setHasPermission] = useState(null);
+const [scanned, setScanned] = useState(false);
+
+useEffect(() => {
+  const getBarCodeScannerPermissions = async () => {
+    const { status } = await BarCodeScanner.requestPermissionsAsync();
+    setHasPermission(status === "granted");
+  };
+
+  getBarCodeScannerPermissions();
+}, []);
+
+const handleBarCodeScanned = ({ type, data }) => {
+  setScanned(true);
+  Linking.openURL(`${data}`);
+};
+
+
+
 
 function Dev({ navigation }) {
   return ( 
@@ -15,40 +34,19 @@ function Dev({ navigation }) {
 }
 
 function Scanner({ navigation }) {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
-
-  useEffect(() => {
-    const getBarCodeScannerPermissions = async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === "granted");
-    };
-
-    getBarCodeScannerPermissions();
-  }, []);
-
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-    Linking.openURL(`${data}`);
-  };
-
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
   }
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
-
+r  
   return (
     <View style={styles.container}>
       <BarCodeScanner
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
       />
-      {scanned && (
-        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
-      )}
     </View>
   );
 }
