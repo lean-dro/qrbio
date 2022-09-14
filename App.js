aa49cc8859a0f9b14da4fd58d58974a2b4382e0f
lean-dro <72211521+lean-dro@users.noreply.github.com>
2022-09-14 08:08:29 -0300

    Drawer para Tabs

--- App.js
+++ App.js
@@ -4,46 +4,64 @@ import { BarCodeScanner } from "expo-barcode-scanner";
 import { Linking } from "react-native";
 import styles from "./assets/css/style";
 import { useFonts } from "expo-font";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-import "react-native-gesture-handler";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 function Dev({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Button
-        onPress={() => navigation.navigate("Scanner")}
-        title="Go to notifications"
-      />
-    </View>
-  );
+  return ( 
+  <View style={styles.container}>
+      <Text style></Text>
+  </View>);
 }
 
 function Scanner({ navigation }) {
- 
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    Linking.openURL(`${data}`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
   return (
     <View style={styles.container}>
-      <Button
-        onPress={() => navigation.navigate("Dev")}
-        title="Go to notifications"
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
       />
-      <Text style={{ fontFamily: "Merienda Bold", fontSize: 30 }}>
-        Leitor de QR-Code
-      </Text>
+      {scanned && (
+        <Button title={"Tap to Scan Again"} onPress={() => setScanned(false)} />
+      )}
     </View>
   );
 }
 
-const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Dev">
-        <Drawer.Screen name="Scanner" component={Scanner} />
-        <Drawer.Screen name="Dev" component={Dev} />
-      </Drawer.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Scanner" component={Scanner} />
+        <Tab.Screen name="Dev" component={Dev} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
@@ -54,5 +72,4 @@ export default function App() {
 
 // npm install @react-navigation/native
 // npx expo install react-native-screens react-native-safe-area-context
-// npm install @react-navigation/drawer
-// npx expo install react-native-gesture-handler react-native-reanimated
+// npm install @react-navigation/bottom-tabs
